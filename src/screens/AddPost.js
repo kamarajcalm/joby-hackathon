2f6e46fcfc6092111486af7f735b56b84c35f02d
Arun Karthick N <arunkarthick.n@siamcomputing.com>
2022-07-03 03:44:00 +0530

    post ui added

---
+++ src/screens/AddPost.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View,Text} from 'native-base'
+const AddPost  =()=>{
+  return(
+    <View>
+      <Text>AddPost</Text>
+    </View>
+  )
+}
+
+export default AddPost
